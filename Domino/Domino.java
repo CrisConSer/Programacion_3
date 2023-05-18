3aa9d19445f7b8ee71637585dc2493a81ec8acaf
CrisConSer <cristinacondeserrano@gmail.com>
2023-05-18 19:32:07 +0200

    ccs

---
+++ Domino/Domino.java
@@ -0,0 +1,29 @@
+/*
+ * Programa para crear fichas de domino
+ * @CrisConSer
+ */
+
+public class Domino{
+
+  public static void main(String[] args){
+    // Crea un array para alojar las 28 fichas del dominó.
+  Ficha[] ficha = new Ficha[28];
+  // Crea las fichas, con ambos valores dados o aleatorios.
+  ficha[0] = new Ficha(6, 1);
+  ficha[1] = new Ficha(0, 4);
+  ficha[2] = new Ficha(3, 3);
+  ficha[3] = new Ficha(0, 1);
+  //Ficha ficha5 = new Ficha();
+  // Imprime todas las fichas creadas
+  for (int i=0; i < Ficha.getNumFichasCreadas(); i++) {
+  System.out.println(ficha[i]);
+  }
+  // voltea() obtiene una nueva ficha con los lados cambiados
+  System.out.println(ficha[1].voltea());
+  // ficha[a].encaja(ficha[b]) es true si ambas fichas tienen algún lado igual
+  System.out.println(ficha[1].encaja(ficha[3]));
+  System.out.println(ficha[0].encaja(ficha[3]));
+  System.out.println(ficha[0].encaja(ficha[2]));
+  System.out.println(ficha[0].encaja(ficha[1]));
+  } 
+}
---
+++ Domino/Ficha.java
@@ -0,0 +1,51 @@
+public class Ficha {
+
+private int lado1;
+private int lado2;
+private static int numFichasCreadas;
+
+// Constructor con ambos lados
+public Ficha(int l1, int l2){
+this.lado1 = l1;
+this.lado2 = l2;
+Ficha.numFichasCreadas++;
+}
+
+/* 
+// Constructor aleatorio
+Ficha(){
+lado1 = (int)(Math.random() * 6 + 1);
+lado2 = (int)(Math.random() * 6 + 1);
+}*/
+
+// Obtiene un nuevo objeto con lados cambiados
+public Ficha voltea() {
+Ficha f = new Ficha(lado1, lado1);
+f.lado1 = lado2;
+f.lado2 = lado1;
+return f;
+}
+
+// Compara si tiene algún lado igual
+boolean encaja(Ficha f){
+return lado1 == f.lado1 || lado1 == f.lado2 ||
+lado2 == f.lado1 || lado2 == f.lado2;
+}
+
+// Obtiene el número de fichas creadas
+
+/*public Ficha (){
+  numFichasCreadas++;
+}*/
+
+public static int getNumFichasCreadas(){
+  return numFichasCreadas;
+}
+
+// Devuelve representación en cadena
+@Override
+  public String toString() {
+    return "[" + (lado1 == 0 ? " " : lado1) + "|" + (lado2 == 0 ? " " : lado2) + ']';
+  }
+}
+
